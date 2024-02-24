12bee6bd5d484ce6108c361f289067952a60bd42
Ziek <zieksanchez3@gmail.com>
2024-02-23 16:06:57 -0800

    Adjust sizing

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import { Cart } from "./Cart";
 
 const Header = () => {
   return (
-    <div className="flex w-5/12 my-8 items-center justify-between">
+    <div className="flex sm:w-8/12 w-10/12 my-8 items-center justify-between">
       <button className="flex px-4 rounded-md transition-all fill-logo-on-hover">
         <Book size={32} className="transition-all" />
         <h1 className="font-display text-2xl font-bold transition-all">
--- src/layouts/Container.tsx
+++ src/layouts/Container.tsx
@@ -5,7 +5,7 @@ const Container = ({ children }: { children: ReactNode }) => {
   return (
     <div className="flex w-full justify-center items-center flex-col h-full">
       <Header />
-      <div className="w-1/4">
+      <div className="sm:w-2/4 w-3/4">
         <p>Top</p>
         {children}
         <p>Bottom</p>
