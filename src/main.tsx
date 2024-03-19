775b31103b85df15848210615a3838f0c4a99a0c
Ziek <zieksanchez3@gmail.com>
2024-03-19 11:52:50 -0700

    Organize code

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
-
-const client = new ApolloClient({
-  uri: import.meta.env.VITE_GRAPHQL_API,
-  cache: new InMemoryCache(),
-});
+import { ApolloProvider } from "@apollo/client";
+import { client } from "./connectors/graphql.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
