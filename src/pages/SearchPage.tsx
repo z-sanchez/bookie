2ae61f9b7d56fdb26af3e8f6b288cb3c6ca49273
Ziek <zieksanchez3@gmail.com>
2024-03-19 12:23:34 -0700

    Add to do list

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -8,6 +8,14 @@ import { Book } from "../types/Book";
 import { useState } from "react";
 
 const SearchPage = () => {
+  //TO DO:
+  /*
+    1. Create search API
+    2. If search term is empty: show 10 results (test)
+    3. Add loader
+    4. Create book component
+    5. Show no books when search returns empty array or fails(test)
+  */
   const theme = useTheme();
   const [searchResults, setSearchResults] = useState<string[]>([]);
   const { data, loading } = useQuery(GET_ALL_BOOKS);
